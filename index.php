5bd4e4771329e1e9cbc7b85af56ea73a6d210f0b
Namdevelop997 <namdevelop997@gmail.com>
2021-06-25 08:03:06 +0700

    demo delete array use unset

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+$arr1 = [
+    [
+        'id' => 1,
+        'fullName' => 'Ma Van Nam',
+        'age' => 18
+    ],
+    [
+        'id' => 2,
+        'fullName' => 'Pham Thi Hoa',
+        'age' => 120
+    ]
+];
+
+echo '<pre>';
+print_r($arr1);
+echo '</pre>';
+
+//delete array use unset 
+unset($arr1[1]);
+echo '<pre>';
+print_r($arr1);
+echo '</pre>';
